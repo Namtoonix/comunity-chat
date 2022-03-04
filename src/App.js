ec0f494d5f50feb95cf694f888e89d7ada474417
Namtoonix <vuhoangnamtnh1998@gmail.com>
2022-03-04 08:24:54 +0700

    Update chat UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
 import { ChatEngine } from "react-chat-engine";
-import "./App.css";
+
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
-App.propTypes = {};
+const projectID = "3a4a529a-932b-40ff-af9e-ad860c4a5bc2";
 
-function App(props) {
+function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
-      projectID="3a4a529a-932b-40ff-af9e-ad860c4a5bc2"
-      userName="admin"
-      userSecret="Xilamtoonix@005882"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/> }
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 }
